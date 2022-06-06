bd868ba13ac2031cb292ac87e25f3fb4fe26d94e
Jordan Shatford <jordanshatford@live.com>
2022-06-06 18:10:25 -0300

    refactor: remove function and place inline

--- utilities/urls.ts
+++ utilities/urls.ts
@@ -1,11 +1,7 @@
-export function removeSpecialCharacters(str: string) {
-  return str.replace(/[&#,+()$~%'.":!*?<>{}]/g, '');
-}
-
 export function toURLSafe(str?: string) {
   if (!str) {
     return '';
   }
-  const s = removeSpecialCharacters(str);
+  const s = str.replace(/[&#,+()$~%'.":!*?<>{}]/g, '');
   return s.replace(/\s+/g, '-').toLowerCase();
 }
